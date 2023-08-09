ab7781aaee38be0779acc640fe2050eeb77e6ba9
shubhashish005 <Undertaker@1991>
2023-08-09 23:27:14 +0530

    Initial Commit

---
+++ src/main/java/solid_principles/singleResponsibility/Account.java
@@ -0,0 +1,34 @@
+package solid_principles.singleResponsibility;
+
+import java.math.BigDecimal;
+
+public class Account {
+
+    private Integer accountNumber;
+    private String firstName;
+    private BigDecimal totalAmount;
+
+    public Integer getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(Integer accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public BigDecimal getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+}
